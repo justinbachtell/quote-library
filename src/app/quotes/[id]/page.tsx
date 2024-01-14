2d8f0aaee0733ea972742dd82d846d8d7e1be92a
Justin Bachtell <justin@justinbachtell.com>
2024-01-14 15:50:39 -0600

    Refactor quote page component

--- src/app/quotes/[id]/page.tsx
+++ src/app/quotes/[id]/page.tsx
@@ -70,64 +70,71 @@ export default async function QuotePage({
 
   return (
     <div className="container my-8 flex flex-col">
-      {sessionUserEmail === env.ADMIN_EMAIL && quote[0]?.id && (
+      {!validUser && quote[0]?.isPrivate && (
+        <div className="mb-8 flex justify-center">
+          <h1>You are not authorized to view this quote, as it is private.</h1>
+        </div>
+      )}
+      {validUser && quote[0]?.id && (
         <div className="mb-8 flex justify-center">
           <EditQuote quoteId={quote[0].id} />
         </div>
       )}
       <Suspense fallback={<h1 className="text-large">Loading...</h1>}>
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col">
-            <h1 className="text-2xl font-bold">
-              Quote from {quote[0]?.bookTitle}&nbsp;by&nbsp;
-              {quote[0]?.quoteAuthors.join(", ")}
-            </h1>
-            <blockquote className="px-8 pt-8 text-xl">
-              <p>"{quote[0]?.text}"</p>
-              <p className="my-4 text-right italic">
-                - {quote[0]?.quoteAuthors.join(", ")}
-              </p>
-            </blockquote>
+        {quote[0]?.id && !quote[0]?.isPrivate && (
+          <div className="flex flex-col gap-8">
+            <div className="flex flex-col">
+              <h1 className="text-2xl font-bold">
+                Quote from {quote[0]?.bookTitle}&nbsp;by&nbsp;
+                {quote[0]?.quoteAuthors.join(", ")}
+              </h1>
+              <blockquote className="px-8 pt-8 text-xl">
+                <p>"{quote[0]?.text}"</p>
+                <p className="my-4 text-right italic">
+                  - {quote[0]?.quoteAuthors.join(", ")}
+                </p>
+              </blockquote>
+            </div>
+            <div className="flex flex-row gap-4">
+              <div className="flex w-1/3 flex-col gap-4">
+                <h2 className="text-xl font-bold">Pages</h2>
+                <p>{quote[0]?.pageNumber}</p>
+              </div>
+              <div className="flex w-2/3 flex-col gap-4">
+                <h2 className="text-xl font-bold">Citation</h2>
+                <p>{quote[0]?.citation}</p>
+              </div>
+            </div>
+            <div className="flex flex-row gap-4">
+              <div className="flex w-1/2 flex-col gap-4">
+                <h2 className="text-xl font-bold">Topics</h2>
+                <ul>
+                  {quoteTopicsArray.map((topic) => (
+                    <li key={topic.id}>{topic.name}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="flex w-1/2 flex-col gap-4">
+                <h2 className="text-xl font-bold">Types</h2>
+                <ul>
+                  {quoteTypesArray.map((type) => (
+                    <li key={type.id}>{type.name}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="flex flex-row gap-4">
+              <div className="flex w-1/2 flex-col gap-4">
+                <h2 className="text-xl font-bold">Tags</h2>
+                <ul>
+                  {quoteTagsArray.map((tag) => (
+                    <li key={tag.id}>{tag.name}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-row gap-4">
-            <div className="flex w-1/3 flex-col gap-4">
-              <h2 className="text-xl font-bold">Pages</h2>
-              <p>{quote[0]?.pageNumber}</p>
-            </div>
-            <div className="flex w-2/3 flex-col gap-4">
-              <h2 className="text-xl font-bold">Citation</h2>
-              <p>{quote[0]?.citation}</p>
-            </div>
-          </div>
-          <div className="flex flex-row gap-4">
-            <div className="flex w-1/2 flex-col gap-4">
-              <h2 className="text-xl font-bold">Topics</h2>
-              <ul>
-                {quoteTopicsArray.map((topic) => (
-                  <li key={topic.id}>{topic.name}</li>
-                ))}
-              </ul>
-            </div>
-            <div className="flex w-1/2 flex-col gap-4">
-              <h2 className="text-xl font-bold">Types</h2>
-              <ul>
-                {quoteTypesArray.map((type) => (
-                  <li key={type.id}>{type.name}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="flex flex-row gap-4">
-            <div className="flex w-1/2 flex-col gap-4">
-              <h2 className="text-xl font-bold">Tags</h2>
-              <ul>
-                {quoteTagsArray.map((tag) => (
-                  <li key={tag.id}>{tag.name}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
+        )}
       </Suspense>
     </div>
   );
